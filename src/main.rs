d6ea4b3f1f94ae2693646ecf3f97708192422c32
Erik Eppel <erik.eppel@trydatabook.com>
2022-05-25 21:29:29 -0700

    (5.4.4) isolates the mantissa

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-const EXPONENT_BIAS: i32 = 127;
-
 fn main() {
     let n: f32 = 42.42;
     let n_bits: u32 = n.to_bits();
-    // Shifts exponent's 8 bits to right, overwriting the mantissa
-    let exponent_ = n_bits >> 23;
-    // Passes `exponent_` through an [AND bit mask](https://en.wikipedia.org/wiki/Mask_(computing)
-    // to set all but the first 8 bits to 0, which effectively removes the sign bit
-    let exponent_ = exponent_ & 0xff;
-    // Casts the exponent and applied the exponent bias to arrive at the final value
-    let exponent = (exponent_ as i32) - EXPONENT_BIAS;
+    let mut mantissa: f32 = 1.0;
 
-    println!("{exponent}");
+    // in an `f32`, the mantissa occupies bits 0..=22
+    for i in 0..23 {
+        // create a mask for a bit position based on `i`
+        let mask = 1 << i;
+        // filter the bits from the original number
+        let one_at_bit_i = n_bits & mask;
+        // if the bit at position `i` is non-zero, continue the decoding process
+        if one_at_bit_i != 0 {
+            let i_ = i as f32;
+            // When `i` is 0, we're rasing 2.0 to the power of -23, which is ~0.0000005
+            let weight = 2_f32.powf(i_ - 23.0);
+            println!("Weight: {}", &weight);
+            // By adding the weight
+            mantissa += weight;
+            println!("Mantissa: {}\n", &mantissa);
+        }
+    }
+
+    println!("Decoded mantissa: {mantissa}");
 }
